00ca4b089252082cf057861168514f6b47a356cc
nekocode <syfyw@qq.com>
2015-10-26 00:30:35 +0800

    add mian func

--- index.py
+++ index.py
@@ -3,7 +3,7 @@
 import os
 import tornado
 from tornado.web import Application, RequestHandler
-from vote_model import SchoolAccount, VoteAccount
+from vote_model import SchoolAccount, VoteAccount, sub_accounts, vote_accounts
 from weixin_helper import xml2dict, WeixinRefreshATKWorker
 
 __author__ = 'nekocode'
@@ -12,24 +12,6 @@ domain = 'http://weixin_vote.tunnel.mobi'
 
 # 排行榜 http://vote.bbdfun.com/uc/top?wechatname=%E5%90%89%E7%8F%A0%E7%9F%A5%E9%81%93
 # http://dushu.xiaomi.com/#1
-
-sub_accounts = {
-    'wxfcc58491aa0b07d6': SchoolAccount({
-        'app_id': 'wxfcc58491aa0b07d6',
-        'app_secret': 'd4624c36b6795d1d99dcf0547af5443d',
-        'token': 'nekocode',
-        'aes_key': "wRR2E0BcY1nrniIe1gf8Otx8DtDG6ibOAYNHZilzakv",
-    }, 'wxfcc58491aa0b07d6')
-}
-
-vote_accounts = {
-    'wxfcc58491aa0b07d6': VoteAccount({
-        'app_id': 'wxfcc58491aa0b07d6',
-        'app_secret': 'd4624c36b6795d1d99dcf0547af5443d',
-        'token': 'nekocode',
-        'aes_key': "wRR2E0BcY1nrniIe1gf8Otx8DtDG6ibOAYNHZilzakv"
-    })
-}
 
 
 class MainHandler(RequestHandler):
@@ -61,22 +43,24 @@ class MainHandler(RequestHandler):
         if appid not in self.accounts:
             self.write('failed')
             return
-        weixin = self.accounts[appid]
+        account = self.accounts[appid]
 
         # msg = xml2dict(weixin.decrypt_xml(self.request.body))  todo: 解密
         msg = xml2dict(self.request.body)
         print msg
+
+        reply_msg = 'success'
         if msg['MsgType'] == 'text':
             user = msg['FromUserName']
             our = msg['ToUserName']
             text = msg['Content']
 
-            if type(weixin) == VoteAccount:
+            if type(account) == VoteAccount:
                 if text.startwith('V'):
-                    vote_rlt = weixin.vote(text)
+                    vote_rlt = account.vote(text)
 
                     if vote_rlt == 0:   # 投票成功
-                        reply_msg = weixin.news_msg(user, our, [{
+                        reply_msg = account.news_msg(user, our, [{
                             'title': '恭喜你为【' + 'xxxx' + '】投票成功!',
                             'description': '查看目前排行榜',
                             'pic_url': 'http://mmbiz.qpic.cn/mmbiz/Wuhx7MUWdrdbtK3wdngeLY5uiaglJm9wi'
@@ -85,64 +69,59 @@ class MainHandler(RequestHandler):
                             'url': 'http://www.baidu.com'
                         }, {
                             'title': '▶点击此处获取邀请码',
-                            'url': domain + '/invite/' + appid  # todo: 外链
+                            'url': domain + '/invite_code/' + account.get_school_account_app_id(text)  # todo: 外链
                         }, {
                             'title': '点击此处查看排行榜~',
-                            'url': domain + '/rank/' + appid
+                            'url': domain + '/rank/' + account.get_school_account_app_id(text)
                         }])
 
                     elif vote_rlt == -1:
-                        reply_msg = weixin.text_msg(user, our, '你的投票码有误，请尝试重新获取')
+                        reply_msg = account.text_msg(user, our, '你的投票码有误，请尝试重新获取')
 
                     elif vote_rlt == -2:
-                        reply_msg = weixin.text_msg(user, our, '你已经在该校投过一次票了，'
-                                                               '快获取邀请码让你的小伙伴们来帮你投票吧')
+                        reply_msg = account.text_msg(user, our, '你已经在该校投过一次票了，'
+                                                                '快获取邀请码让你的小伙伴们来帮你投票吧')
 
                 elif text.startwith('Q'):
                     # todo：提供所在学校的链接
-                    reply_msg = weixin.text_msg(user, our, '请将邀请码发给你的小伙伴，'
-                                                           '并在举办你所在学校比赛的子公众号内使用邀请码投票')
+                    reply_msg = account.text_msg(user, our, '请将邀请码发给你的小伙伴，'
+                                                            '并在举办你所在学校比赛的子公众号内使用邀请码投票')
 
-                else:
-                    reply_msg = 'success'
-
-            elif type(weixin) == SchoolAccount:
+            elif type(account) == SchoolAccount:
                 if text.startwith('V'):
-                    reply_msg = weixin.news_msg(user, our, [{
+                    reply_msg = account.news_msg(user, our, [{
                         'title': '为防止刷票，请到该公众号下投票',
                         'url': 'http://www.baidu.com'
                     }])
 
                 elif text.startwith('C'):
-                    reply_msg = weixin.news_msg(user, our, [{
+                    reply_msg = account.news_msg(user, our, [{
                         'title': '你还差一步即可成功为【' + 'xxxx' + '】投票',
                         'description': '查看目前排行榜',
-                        'pic_url': 'http://mmbiz.qpic.cn/mmbiz/Wuhx7MUWdrdbtK3wdngeLY5uiaglJm9wibN'
-                                    'gMzWB0WJS1HsOTomORJia3ibIJlJGCYFXzofdM6o4yXEXIUPic4oux0w/640?wx_'
-                                    'fmt=jpeg&tp=webp&wxfrom=5',  # todo：内链模板
+                        'pic_url': 'http://mmbiz.qpic.cn/mmbiz/Wuhx7MUWdrdbtK3wdngeLY5uiaglJm9wibNgMzWB0W'
+                                   'JS1HsOTomORJia3ibIJlJGCYFXzofdM6o4yXEXIUPic4oux0w/640?wx_fmt=jpeg&tp=w'
+                                   'ebp&wxfrom=5',  # todo：内链模板
                         'url': 'http://www.baidu.com'
                     }, {
                         'title': '▶点击此处完成投票操作',
-                        'url': domain + '/vote/' + appid  # todo: 外链
+                        'url': domain + '/vote_code/' + appid  # todo: 外链
                     }, {
                         'title': '点击此处查看排行榜~',
                         'url': domain + '/rank/' + appid
                     }])
 
-            else:
-                reply_msg = 'success'
-
-            self.write(reply_msg)
-
-        else:
-            self.write('success')
+        self.write(reply_msg)
 
 
-class VoteHandler(RequestHandler):
+class VoteCodeHandler(RequestHandler):
     def data_received(self, chunk):
         pass
 
     def get(self, app_id):
+        if app_id not in sub_accounts:
+            self.write('failed')
+
+        account = sub_accounts[app_id]
         self.write('为了防止刷票，你的验证码是 V12345')
 
 
@@ -151,14 +130,22 @@ class RankHandler(RequestHandler):
         pass
 
     def get(self, app_id):
+        if app_id not in sub_accounts:
+            self.write('failed')
+
+        account = sub_accounts[app_id]
         self.write('排行榜：' + app_id)
 
 
-class InviteHandler(RequestHandler):
+class InviteCodeHandler(RequestHandler):
     def data_received(self, chunk):
         pass
 
     def get(self, app_id):
+        if app_id not in sub_accounts:
+            self.write('failed')
+
+        account = sub_accounts[app_id]
         self.write('你的邀请码')
 
 settings = {
@@ -168,13 +155,14 @@ settings = {
 application = Application([
     (r'/sub_account/(.*)', MainHandler, dict(accounts=sub_accounts, is_vote_account=False)),
     (r'/vote_account/(.*)', MainHandler, dict(accounts=vote_accounts, is_vote_account=True)),
-    (r'/vote/(.*)', VoteHandler),
+    (r'/vote_code/(.*)', VoteCodeHandler),
     (r'/rank/(.*)', RankHandler),
-    (r'/invite/(.*)', InviteHandler)
+    (r'/invite_code/(.*)', InviteCodeHandler)
 ], **settings)
 
 
 if __name__ == '__main__':
+    # 刷新 access token
     # for key, invite_weixin in invite_account.items():
     #     WeixinRefreshATKWorker(invite_weixin).start()
     #
--- vote_model.py
+++ vote_model.py
@@ -4,20 +4,38 @@ __author__ = 'nekocode'
 
 
 class VoteAccount(WeixinHelper):
-    def __init__(self, account, school_accounts=None):
-        WeixinHelper.__init__(self, account)
-        self.school_accounts = school_accounts if school_accounts is not None else dict()
+    def __init__(self, account_config):
+        WeixinHelper.__init__(self, account_config)
+
+        self.school_accounts = dict()
 
     def vote(self, vote_code):
         # todo：做投票操作，并返回相应的错误代码
         pass
 
+    def get_school_account_app_id(self, vote_code):
+        pass
+
+    def get_classes_rank(self):
+        pass
+
+    def get_person_rank(self):
+        pass
+
 
 class SchoolAccount(WeixinHelper):
-    def __init__(self, account, vote_account_id, classes=None):
-        WeixinHelper.__init__(self, account)
+    def __init__(self, account_config, vote_account_id):
+        WeixinHelper.__init__(self, account_config)
+
         self.vote_account_id = vote_account_id
-        self.classes = classes if classes is not None else dict()
+        self.classes = dict()
+
+    def get_vote_code(self):
+        # todo：获取投票码，并返回相应的错误代码
+        pass
+
+    def get_invite_code(self):
+        pass
 
     def get_classes_rank(self):
         pass
@@ -27,15 +45,35 @@ class SchoolAccount(WeixinHelper):
 
 
 class Class:
-    def __init__(self, class_id, sub_account_id, voting_count=0, voted_peoples=None):
+    def __init__(self, class_id):
         self.class_id = class_id
-        self.school = sub_account_id
-        self.voting_count = voting_count
-        self.voted_peoples = voted_peoples if voted_peoples is not None else dict()
+
+        self.voting_count = 0
+        self.voting_log = dict()
 
 
 class People:
     def __init__(self, people_id, voting_count=0):
         self.people_id = people_id
         self.voting_count = voting_count
+
+
+sub_accounts = {
+    '': SchoolAccount({
+        'app_id': '',
+        'app_secret': '',
+        'token': 'nekocode',
+        'aes_key': "",
+    }, vote_account_id='wxfcc58491aa0b07d6')
+}
+
+vote_accounts = {
+    'wxfcc58491aa0b07d6': VoteAccount({
+        'app_id': 'wxfcc58491aa0b07d6',
+        'app_secret': 'd4624c36b6795d1d99dcf0547af5443d',
+        'token': 'nekocode',
+        'aes_key': "wRR2E0BcY1nrniIe1gf8Otx8DtDG6ibOAYNHZilzakv"
+    })
+}
+
 
